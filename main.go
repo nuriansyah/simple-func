14cc7a6f7ec3ec35177a9e8f620caf4b6c2b1c21
Muhammad Nuriansyah Malik Nugraha <nuriansyah09@gmail.com>
2023-07-26 11:23:41 +0700

    add:"Function Append String And Print"

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	readPrint()
+	appendStringAndPrint()
 }
 func readPrint() {
 	var inputString1, inputString2 string
@@ -12,4 +13,17 @@ func readPrint() {
 	fmt.Print("Enter the second string: ")
 	fmt.Scanln(&inputString2)
 	fmt.Println(inputString1, inputString2)
+}
+
+func appendStringAndPrint() {
+	var inputString1, inputString2 string
+
+	fmt.Print("Enter the first string: ")
+	fmt.Scanln(&inputString1)
+	fmt.Print("Enter the second string: ")
+	fmt.Scanln(&inputString2)
+
+	appendedString := inputString1 + inputString2
+
+	fmt.Println("Appended string:", appendedString)
 }
